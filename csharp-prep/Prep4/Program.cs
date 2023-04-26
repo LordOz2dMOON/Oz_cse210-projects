57980677aff5526671c2923ea8aede6fdbbc9565
LordOz2dMOON <oswaldojo@ymail.com>
2023-04-27 00:23:13 +0100

    solution for collection assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+      Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+      //string userInput = Console.ReadLine();
+
+      int number = 4;
+      List <int> listOfNumbers = new List<int>();
+      while (number != 0)
+      {
+        Console.Write("Enter number: ");
+        string listItem = Console.ReadLine();
+        number = int.Parse(listItem);
+        listOfNumbers.Add(number);
+        if (number == 0)
+        {
+          int sum = 0;
+          int listCount = listOfNumbers.Count - 1;
+          int max = listOfNumbers.Max();
+          double average = 0.0;
+          for (int i = 0; i < listOfNumbers.Count; i++)
+          {
+            sum += listOfNumbers[i];
+            average = sum / listCount;
+          }
+          Console.WriteLine($"The sum is: {sum}");
+          Console.WriteLine($"The average is: {average}");
+          Console.WriteLine($"The largest number is {max}");
+          break;
+        }
+      }
     }
 }
