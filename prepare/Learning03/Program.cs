5e3357021c1f628eb34e77e06e4f4b08f5faea1e
LordOz2dMOON <oswaldojo@ymail.com>
2023-05-19 04:34:30 +0100

    Encapsulation learning activity solution

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction number = new Fraction();
+        Fraction numberOne = new Fraction(1);
+        Fraction numberTwo = new Fraction(5);
+        Fraction numberThree = new Fraction(3, 4);
+        Fraction numberFour = new Fraction(1,3);
+
+        Console.WriteLine(number.GetFractionString());
+        Console.WriteLine(number.GetTop());
+        Console.WriteLine(numberTwo.GetFractionString());
+        Console.WriteLine(numberTwo.GetTop());
+        Console.WriteLine(numberThree.GetFractionString());
+        Console.WriteLine(numberThree.GetDecimalValue());
+        Console.WriteLine(numberFour.GetFractionString());
+        Console.WriteLine(numberFour.GetDecimalValue());
+
     }
 }
