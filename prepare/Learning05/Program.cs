436d78429750f77253c4892a1295e0ffcecf97bb
LordOz2dMOON <oswaldojo@ymail.com>
2023-06-14 07:19:43 +0100

    Learning 05 polymorphism activity solution

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square small = new Square("red",4);
+        Rectangle bigger = new Rectangle("Pink", 5, 10);
+        Circle round = new Circle("blue", 5);
+        shapes.Add(small);
+        shapes.Add(bigger);
+        shapes.Add(round);
+
+        foreach(Shape shape in shapes)
+        {
+        string color = shape.GetColor();
+        double area = shape.GetArea();
+        Console.WriteLine(color);
+        Console.WriteLine(area);
+        }
     }
 }
